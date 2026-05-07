904add192c62352469c92cdf730763922abea4e0
mmmariano <mtemariano@gmail.com>
2026-05-07 16:23:41 -0300

    Update components.js

--- components.js
+++ components.js
@@ -69,7 +69,7 @@ const footerHTML = `
             </div>
             <div class="footer-column">
                 <h4>Contato</h4>
-                <a href="https://wa.me/5544999761842">WhatsApp</a>
+                <a href="https://wa.me/5544999761842?text=Olá,%20necessito%20de%20um%20orçamento%20da%20TryWebDesign">WhatsApp</a>
                 <a href="mailto:atendimentotrywebdesign@gmail.com">E-mail</a>
                 <a href="/#contato">Consultoria</a>
             </div>
@@ -92,7 +92,7 @@ const footerHTML = `
 
 // ✅ Declarada ANTES da função que a usa
 const whatsappFluanteHTML = `
-<a href="https://wa.me/5544999761842" class="whatsapp-fixo-small" target="_blank" aria-label="Chat no WhatsApp" style="
+<a href="https://wa.me/5544999761842?text=Olá,%20necessito%20de%20um%20orçamento%20da%20TryWebDesign" class="whatsapp-fixo-small"  target="_blank" aria-label="Chat no WhatsApp" style="
     position: fixed;
     bottom: 24px;
     right: 24px;
