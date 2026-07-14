831db3f7b5d8c8032b6ed6e081af39d1cb925a87
mmmariano <mtemariano@gmail.com>
2026-07-13 21:51:39 -0300

    Update components.js

--- components.js
+++ components.js
@@ -77,7 +77,7 @@ const footerHTML = `
                 <h4>Social</h4>
                 <a href="https://www.instagram.com/try.web.design/" target="_blank">Instagram</a>
                 <a href="https://www.facebook.com/profile.php?id=61579022016363" target="_blank">Facebook</a>
-                <a href="#" target="_blank">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/mateusmariano/" target="_blank">LinkedIn</a>
             </div>
         </div>
     </div>
